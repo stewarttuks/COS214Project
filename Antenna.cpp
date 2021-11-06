47484262aec3dbb7c20267880cf01de16f3f1230
Ashleigh-Tuks <u20528834@tuks.co.za>
2021-11-06 12:56:58 +0200

    Observer Pattern

---
+++ Antenna.cpp
@@ -0,0 +1,27 @@
+#include "Antenna.h"
+
+Antenna::Antenna(){
+    signal = "";
+}
+
+Antenna::~Antenna(){
+    for (auto i = satelliteList.begin(); i != satelliteList.end(); i++)
+        delete *i;
+
+}
+
+void Antenna::setSatelliteList(vector<Satellite*> s){
+    satelliteList = s;
+}
+
+vector<Satellite*> Antenna::getSatelliteList(){
+    return satelliteList;
+}
+
+void Antenna::setSignal(string s){
+    signal = s;
+}
+
+string Antenna::getSignal(){
+    return signal;
+}
---
+++ Antenna.h
@@ -0,0 +1,33 @@
+#ifndef ANTENNA_H
+#define ANTENNA_H
+
+#include <iostream>
+#include <vector>
+#include "Satellite.h"
+
+class Satellite;
+using namespace std;
+
+class Antenna
+{
+    private:
+        vector<Satellite*> satelliteList;
+        string signal;
+    public:
+        Antenna();
+        ~Antenna();
+
+        virtual void attach(Satellite*) = 0;
+        virtual void detach(Satellite*) = 0;
+        virtual void notify() = 0;
+
+
+        //setters and getters
+        string getSignal();
+        void setSignal(string);
+        vector<Satellite*> getSatelliteList();
+        void setSatelliteList(vector<Satellite*>);
+
+};
+
+#endif
