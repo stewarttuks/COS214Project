c617e13764fee9a2e85857ca10a280a13a919e91
Stewart Coetzee <u20430630@tuks.co.za>
2021-11-22 16:55:01 +0200

    General updates 2

--- Satellite.cpp
+++ Satellite.cpp
@@ -6,23 +6,59 @@
  * @date 2021-11-22
  */
 #include "Satellite.h"
-
+ 
+/**
+ * Constructor
+ * @author Runtime Terror
+ * @param[in] i  id of the satellite
+ */
 Satellite::Satellite(int i){
     id = i;
 }
-
+ 
+/**
+ * Destructor
+ * @author Runtime Terror
+ */
 Satellite::~Satellite(){
     id = 0;
 }
-
+ 
+/**
+ * Returns the ID.
+ * @author Runtime Terror
+ * @return id  int  ID of the satellite
+ */
 int Satellite::getid(){
     return id;
 }
-
+ 
+/**
+ * sets the ID
+ * @author Runtime Terror
+ * @param[in] i  ID to be set
+ * @return void
+ */
 void Satellite::setid(int i){
     id = i;
 }
-
+ 
+// Antenna* Satellite::getantenna(){
+//     return antenna;
+// }
+ 
+// void Satellite::setantenna(Antenna* a){
+//     antenna = a;
+// }
+ 
+/**
+ * Receives a message from SpaceX Station
+ * @author Runtime Terror
+ * @param[in] message  The message being received.
+ * @return void
+ */
 void Satellite::receiveMessage(string message){
     cout << "Satellite " << id << " has received message from SpaceX Station: " << message << endl;
 }
+ 
+
